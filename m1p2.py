cbf593136ef54c8c71cedc36094aa0d59c6706a5
Unniro <Unniro@yandex.ru>
2024-10-20 22:52:48 +0800

    Update m1p2.py

--- m1p2.py
+++ m1p2.py
@@ -1,5 +1,5 @@
-homework_completed=12
-hours_spent=1.5
-course_name='"Pyhton"'
-time_of_one_quest=hours_spent/homework_completed
+homework_completed = 12
+hours_spent = 1.5
+course_name = '"Pyhton"'
+time_of_one_quest = hours_spent / homework_completed
 print('На',homework_completed,'рабочих заданий по курсу',course_name,'затрачено',hours_spent,'часа. Итого, выполнение одного задания занимает',time_of_one_quest,'часа')
