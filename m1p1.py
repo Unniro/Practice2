adcfe3a008bc5decdd19e833d7dc704957154dbf
Андрей <unniro@yandex.ru>
2024-10-27 01:09:29 +0800

    Update m1p1.py

--- m1p1.py
+++ m1p1.py
@@ -1,8 +1,8 @@
 name = "Андрей"
-print('My name is',name)
+print('My name is', name)
 age = 42
-print('My age is',age)
-newage = age+1
-print('Next year I will be',newage,'years old')
+print('My age is', age)
+newage = age + 1
+print('Next year I will be', newage, 'years old')
 is_student = True
-print('Is_student:',is_student)
+print('Is_student:', is_student)
