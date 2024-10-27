bfbdda5d20536027e5f81a8effc5dcc3e8771bc7
Андрей <unniro@yandex.ru>
2024-10-27 12:19:56 +0800

    Update m1p1.py

---
+++ m2p3.py
@@ -0,0 +1,12 @@
+# Задача "Нули ничто, отрицание недопустимо!"
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while my_list[i] >= 0:
+    if my_list[i] == 0:
+        i = i + 1
+        continue
+    else:
+        print(my_list[i])
+        i = i + 1
+    if i == len(my_list):
+        break
