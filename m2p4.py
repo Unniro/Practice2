d2883ae1b068f21efd4ed00bce9b42cdd2eaae72
Андрей <unniro@yandex.ru>
2024-11-05 21:13:46 +0800

    Update m1p1.py

--- m2p4.py
+++ m2p4.py
@@ -3,11 +3,15 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for i in range(len(numbers)):
-    for j in range(len(numbers)):
-        print(i, j, numbers[i], numbers[j])
-        if numbers[i] % numbers[j] == 0 and i < j:
-            primes.append(numbers[i])
-        else:
-            not_primes.append(numbers[i])
+    is_prime = True
+    for j in range(2, numbers[i] - 1):
+        if numbers[i] % j == 0:
+            is_prime = False
+    if numbers[i] == 0 or numbers[i] == 1:
+        continue
+    if is_prime is True:
+        primes.append(numbers[i])
+    else:
+        not_primes.append(numbers[i])
 print('Primes: ', primes)
 print('Not Primes: ', not_primes)
