d68c1a6b04c0c1d415f28492e9deda34bd0f51de
Андрей <unniro@yandex.ru>
2024-11-12 00:11:42 +0800

    Update m1p1.py

--- m2p6.py
+++ m2p6.py
@@ -1,14 +1,14 @@
 #Задание "Слишком древний шифр"
-digit = int(input('Введите число изображенное на первой вставке (От 3 до 20): '))
-while digit < 3 or digit > 20:
-    print('Вы ввели неверное число, повторите ввод')
+n = int(input('Введите число из первой вставки (От 3 до 20): '))
+while n < 3 or n > 20:
+    print('Вы ввели неверное число, повторите ввод!')
+    n = int(input('Введите число из первой вставки (От 3 до 20): '))
 result = []
-for i in range(1, digit // 2 + 1):
-    for j in range(1, 20):
-        if digit % (i + j) == 0:
+for i in range(1, n // 2 + 1):
+    for j in range(i + 1, 20):
+        if n % (i + j) == 0:
             result.append(i)
         else:
             continue
         result.append(j)
-print('Ваш пароль:', *result)
-
+print('Ваш пароль:', *result, sep = '')
