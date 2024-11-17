d2ce7583aec52c2b5cc1e8591c22b803a16831a0
Андрей <unniro@yandex.ru>
2024-11-18 00:52:22 +0800

    Update m1p1.py

---
+++ m3p2.py
@@ -0,0 +1,17 @@
+#Задача "Рассылка писем"
+def send_email(message, recipient, sender = 'university.help@gmail.com'):
+    rashir = ['.com', '.ru', '.net']
+    #if not any(item in str(recipient.endswith) for item in rashir):
+    if not recipient.endswith('.com') and recipient.endswith('.ru') and recipient.endswith('.net') and sender.endswith('.com') and sender.endswith('.ru') and sender.endswith('.net'):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        print(str(recipient.endswith))
+    elif sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+    elif sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}')
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
