c10d89c77597f4d22b61acc1a3ef7b1c2b18ab99
Андрей <unniro@yandex.ru>
2024-11-28 22:43:14 +0800

    My works updated...

--- m3p6.py
+++ m3p6.py
@@ -2,15 +2,19 @@
 def calculate_structure_sum(args):
     summa = 0
     for i in args:
+        print(i)
         if isinstance(i, str) == True:
             summa = len(i)
-            return summa + calculate_structure_sum(i - 1)
+            return summa + calculate_structure_sum(i-1)
         elif isinstance(i, int) == True:
             summa += i
-            return summa + calculate_structure_sum(i - 1)
+            return summa + calculate_structure_sum(i-1)
         elif isinstance(i, list) == True:
             summa = sum(i)
-            return summa + calculate_structure_sum(i - 1)
+            return summa + calculate_structure_sum(i-1)
+        elif isinstance(i, dict) == True:
+            summa = sum(i)
+            return summa + calculate_structure_sum(i-1)
 
 
 data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}), "Hello",
