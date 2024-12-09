f5fcf6e1021eb4e982912e8c0d785ea7a8953817
Андрей <unniro@yandex.ru>
2024-12-10 01:26:44 +0800

    My works updated...

---
+++ Module 4/module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+#inner_function() - При вызове вложенной функции, её имя не определяется,
+#                   так как она в объемлющем пространстве имен функции test_function
+#                   и может быть вызвана только внутри нее
+test_function()
